08ee6c49a1d974e9efcd78bd28fd161c71fc5f3f
MehrdadFalahati <m.falahati2022@gmail.com>
2023-06-26 20:57:37 +0330

    feat: implemented valid parentheses tests

--- src/test/java/com/mehrdad/falahati/algorithms/ValidParenthesesSolutionTest.java
+++ src/test/java/com/mehrdad/falahati/algorithms/ValidParenthesesSolutionTest.java
@@ -1,7 +1,27 @@
 package com.mehrdad.falahati.algorithms;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidParenthesesSolutionTest {
+
+    @Test
+    void test_is_valid_by_first_example() {
+        var vp = new ValidParenthesesSolution();
+        assertTrue(vp.isValid("(){}[]"));
+    }
+
+    @Test
+    void test_is_valid_by_second_example() {
+        var vp = new ValidParenthesesSolution();
+        assertFalse(vp.isValid("(({]]{"));
+    }
+
+    @Test
+    void test_is_valid_by_third_example() {
+        var vp = new ValidParenthesesSolution();
+        assertFalse(vp.isValid("(()"));
+    }
 
 }
