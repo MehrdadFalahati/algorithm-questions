c4b1b60d708367aa959c7dea9138a4395bf55cb9
MehrdadFalahati <m.falahati2022@gmail.com>
2023-06-27 20:37:12 +0330

    feat: implemented container with most water

---
+++ src/main/java/com/mehrdad/falahati/algorithms/ContainerWithMostWaterSolutions.java
@@ -0,0 +1,22 @@
+package com.mehrdad.falahati.algorithms;
+
+/**
+ * leetcode number 11
+ * <a href="https://leetcode.com/problems/container-with-most-water/">leetcode11</a>
+ */
+public class ContainerWithMostWaterSolutions {
+
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] <= height[right])
+                left++;
+            else
+                right--;
+        }
+        return max;
+    }
+}
---
+++ src/test/java/com/mehrdad/falahati/algorithms/ContainerWithMostWaterSolutionsTest.java
@@ -0,0 +1,20 @@
+package com.mehrdad.falahati.algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerWithMostWaterSolutionsTest {
+
+    @Test
+    void test_max_area_by_first_example() {
+        var cwmw = new ContainerWithMostWaterSolutions();
+        assertEquals(49, cwmw.maxArea(new int[] {1,8,6,2,5,4,8,3,7}));
+    }
+
+    @Test
+    void test_max_area_by_second_example() {
+        var cwmw = new ContainerWithMostWaterSolutions();
+        assertEquals(1, cwmw.maxArea(new int[] {1,1}));
+    }
+}
