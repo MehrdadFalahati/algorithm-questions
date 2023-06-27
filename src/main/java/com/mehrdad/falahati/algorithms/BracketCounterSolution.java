7f438a4893635f1836189f0f262ea54b667d401e
MehrdadFalahati <m.falahati2022@gmail.com>
2023-06-27 19:58:41 +0330

    feat: implemented brackets counter

---
+++ src/main/java/com/mehrdad/falahati/algorithms/BracketCounterSolution.java
@@ -0,0 +1,30 @@
+package com.mehrdad.falahati.algorithms;
+
+import java.util.Stack;
+
+public class BracketCounterSolution {
+
+    public int countBrackets(String words) {
+        if (words == null || words.isEmpty())
+            return -1;
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        for (var ch : words.toCharArray()) {
+            if (indexOfBrackets(ch) == 1)
+                stack.push(ch);
+            else if (!stack.isEmpty() && (indexOfBrackets(stack.peek()) + 1) == indexOfBrackets(ch)) {
+                stack.pop();
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int indexOfBrackets(char ch) {
+        return switch (ch) {
+            case '[' -> 1;
+            case ']' -> 2;
+            default -> -1;
+        };
+    }
+}
---
+++ src/test/java/com/mehrdad/falahati/algorithms/BracketCounterSolutionTest.java
@@ -0,0 +1,32 @@
+package com.mehrdad.falahati.algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BracketCounterSolutionTest {
+
+    @Test
+    void test_count_brackets_by_first_example() {
+        var bc = new BracketCounterSolution();
+        assertEquals(1, bc.countBrackets("you can find this [it is a test]"));
+    }
+
+    @Test
+    void test_count_brackets_by_second_example() {
+        var bc = new BracketCounterSolution();
+        assertEquals(3, bc.countBrackets("you can [find] this [[it is a test]]"));
+    }
+
+    @Test
+    void test_count_brackets_by_third_example() {
+        var bc = new BracketCounterSolution();
+        assertEquals(3, bc.countBrackets("you can [[find] this [[it is a test]]"));
+    }
+
+    @Test
+    void test_count_brackets_by_forth_example() {
+        var bc = new BracketCounterSolution();
+        assertEquals(0, bc.countBrackets("[[["));
+    }
+}
